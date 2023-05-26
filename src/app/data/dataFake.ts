1d37cec78b16c00c4bf5f77c0d145802d196c890
christianebs <christianebs90@gmail.com>
2023-05-26 17:14:56 -0300

    updating project

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "title": "'A Pequena Sereia' estreia nos cinemas",
+      "description": "O live-action da clássica animação da Disney, 'A Pequena Sereia', estrou nesta quinta-feira (25) nos cinemas. No filme, a jovem sereia Ariel (Halle Bailey) salvou o Príncipe Eric (Jonah Hauer-King) e ficou encantada por ele.",
+      "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/a_pequena_sereia_cartaes_conectam__x2326f33l-1210x544.jpg"
+    },
+    {
+      "id":"2",
+      "title": "Velozes e Furiosos 10 desbanca Guardiões 3 e assume topo das bilheterias",
+      "description": "blablabla",
+      "photoCover":"https://i.ytimg.com/vi/ByGA7aWSxW4/maxresdefault.jpg"
+    }
+  ]
